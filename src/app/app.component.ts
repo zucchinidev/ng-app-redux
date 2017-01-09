323954275d6258b40398cfed47bd2bc564819ad5
Andrea Zucchini <azga84@gmail.com>
2017-01-10 00:11:44 +0100

    create app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Item, AppStore } from './domain/models/index';
+import { ItemsService } from './domain/services/items.service';
+import { Store } from '@ngrx/store';
+import { SELECT_ITEM } from './domain/reducers/selectedItem';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Angular 2 && Redux!';
+  items: Observable<Array<Item>>;
+  selectedItem: Observable<Item>;
+
+  constructor(private itemsService: ItemsService, private store: Store<AppStore>) {
+    this.items = this.itemsService.items;
+    this.selectedItem = this.store.select('selectedItem');
+    this.selectedItem.subscribe(console.log.bind(this));
+    this.itemsService.loadItems();
+  }
+
+  selectItem(item: Item) {
+    this.store.dispatch({
+      type: SELECT_ITEM,
+      payload: item
+    });
+  }
+
+  deleteItem(item: Item) {
+    this.itemsService.deleteItem(item);
+  }
+
+  saveItem(item: Item) {
+
+    this.itemsService.saveItem(item);
+    this.resetItem();
+  }
+
+  resetItem() {
+    let emptyItem: Item = {id: null, name: '', description: ''};
+    this.store.dispatch({
+      type: SELECT_ITEM,
+      payload: emptyItem
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
+import { ItemsListComponent } from './items-list/items-list.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
+import { itemsReducer, selectedItemReducer } from './domain/reducers/index';
+import { ItemsService } from './domain/services/items.service';
+import { StringService } from './domain/services/string.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsListComponent,
+    ItemDetailsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    StoreModule.provideStore({
+      itemsReducer,
+      selectedItemReducer
+    }) // The store that defines our app state
   ],
-  providers: [],
+  providers: [ItemsService, StringService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

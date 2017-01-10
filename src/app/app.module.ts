a88c06982667155118154d173c9fc929f41fd403
Andrea Zucchini <azga84@gmail.com>
2017-01-10 21:42:53 +0100

    Change property names reducers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
 import { ItemsListComponent } from './items-list/items-list.component';
 import { ItemDetailsComponent } from './item-details/item-details.component';
-import { itemsReducer, selectedItemReducer } from './domain/reducers/index';
+import { itemsReducer, selectedItemReducer } from './domain/reducers';
 import { ItemsService } from './domain/services/items.service';
 import { StringService } from './domain/services/string.service';
 
@@ -22,8 +22,8 @@ import { StringService } from './domain/services/string.service';
     FormsModule,
     HttpModule,
     StoreModule.provideStore({
-      itemsReducer,
-      selectedItemReducer
+      items: itemsReducer,
+      selectedItem: selectedItemReducer
     }) // The store that defines our app state
   ],
   providers: [ItemsService, StringService],
